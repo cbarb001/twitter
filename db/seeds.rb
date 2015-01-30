e8f483d53c849cf7ebf5d5cbc80900a9eed9fc36
Cosimo Barbato <cbarb001@gmail.com>
2015-01-30 08:36:04 -0800

    follow button

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
+# Users
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -22,8 +23,18 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+# Microposts
+
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
